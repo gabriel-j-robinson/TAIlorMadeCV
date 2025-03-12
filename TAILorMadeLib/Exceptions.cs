7ade2a364abee23a28d234d6e14d427b73aec484
Gabriel <gabrielrobinson285@gmail.com>
2025-03-12 15:52:49 -0600

    exceptions for text extraction

---
+++ TAILorMadeLib/Exceptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAILorMadeLib
+{
+    public class BadFileException : Exception
+    {
+        public BadFileException() { }
+
+        public BadFileException(string message) : base(message) { }
+
+        public BadFileException(string message, Exception inner) : base(message, inner) { }
+    }
+
+    public class UnsupportedFileException : Exception
+    {
+        public UnsupportedFileException() { }
+        public UnsupportedFileException(string message) : base(message) { }
+
+        public UnsupportedFileException(string message, Exception inner) : base(message, inner) { }
+    }
+}
