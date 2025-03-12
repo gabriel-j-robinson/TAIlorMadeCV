ba352eead2bdd0bc24f423d9372da7eb0ca4ab35
Gabriel <gabrielrobinson285@gmail.com>
2025-03-12 14:19:00 -0600

    unit tests

---
+++ TAIlorMadeLibTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TAIlorMadeLibTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TAIlorMadeLibTests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c19b5902-83be-4a52-87ea-6f6cd34ab347")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TAIlorMadeLibTests/TextExtractionTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TAIlorMadeLibTests
+{
+    [TestClass]
+    public class TextExtractionTests
+    {
+        [TestMethod]
+        public void PdfTextExtractionTest1()
+        {
+        }
+
+        [TestMethod]
+        public void DocxTextExtractionTest1()
+        {
+        }
+    }
+}
